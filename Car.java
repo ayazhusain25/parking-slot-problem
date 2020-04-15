6c7c02459c2c0e1444b7308c3c5133f38b9c7e27
ayazhusain25 <63739932+ayazhusain25@users.noreply.github.com>
2020-04-15 22:39:45 +0530

    Bean class

---
+++ Car.java
@@ -0,0 +1,10 @@
+package com.parkingLot;
+
+public class Car {
+	 String regNo;
+     String color;
+     public Car(String regNo, String color) {
+         this.regNo = regNo;
+         this.color = color;
+     }
+}
