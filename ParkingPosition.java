7476353721865b6a4d858bf93a626f27b26acb98
ayazhusain25 <63739932+ayazhusain25@users.noreply.github.com>
2020-04-15 22:51:53 +0530

    class with main method

--- ParkingPosition.java
+++ ParkingPosition.java
@@ -6,5 +6,5 @@ public class ParkingPosition {
 	  public static void main( String args[]) throws FileNotFoundException {
 		  InputParser obj= new InputParser(new ParkingLot());
 		  obj.parseFileInput("C:\\Users\\91980\\command.txt");
-	  }
+	  } 
 }
